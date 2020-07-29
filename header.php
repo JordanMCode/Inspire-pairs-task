33a7860010f5f0e724da0ccbe22d46c7bb925e3a
Adam Ferguson <61731043+AFerguson01@users.noreply.github.com>
2020-07-29 11:56:11 +0100

    Added Landing Page

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
         <img src="assets/imgs/logo.jpg" alt="Logo" id="main-logo">
         <a class="nav-link" href="./index.php?page=pages/about-us.php">About Us</a>
         <a class="nav-link" href="./index.php?page=pages/our-services.php">Our Services</a>
-        <a class="nav-link" href="./index.php?page=./pages/testimonials.php">Testimonials</a> 
-        <a class="nav-link" href="./index.php?page=./pages/contact-us.php">Contact Us</a>
+        <a class="nav-link" href="./index.php?page=pages/testimonials.php">Testimonials</a> 
+        <a class="nav-link" href="./index.php?page=pages/contact-us.php">Contact Us</a>
     </div>
 </header>
