8c1b4f61b851bda905edc82e389a08b17dae972f
JordanMCode <66785212+JordanMCode@users.noreply.github.com>
2020-07-29 12:05:48 +0100

    Pull test

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
 <header>
-    <div>
-        <h1>This is a header.</h1>
+    <div class="header-container">
+        <div class=" container header-items">
         <img src="assets/imgs/logo.jpg" alt="Logo" id="main-logo">
         <a class="nav-link" href="./index.php?page=pages/about-us.php">About Us</a>
         <a class="nav-link" href="./index.php?page=pages/our-services.php">Our Services</a>
         <a class="nav-link" href="./index.php?page=./pages/testimonials.php">Testimonials</a> 
         <a class="nav-link" href="./index.php?page=./pages/contact-us.php">Contact Us</a>
+        </div>
     </div>
 </header>
