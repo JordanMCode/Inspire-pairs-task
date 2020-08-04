f9ebb875b0e3e162c0465dca8771059193e26fca
JordanMCode <66785212+JordanMCode@users.noreply.github.com>
2020-08-04 14:21:14 +0100

    Our services internal page

--- pages/our-services.php
+++ pages/our-services.php
@@ -1,3 +1,32 @@
-<div>
-    <h1>Our Services</h1>
+<div class="our-services">
+    <<div class="internal-page-banner our-services-img">
+
+    </div>
+    <div class="our-services-header">
+        <h1>Our Services</h1>
+    </div>
+    <div class="container purple-bg flex">
+        <h3>Apprenticeship Recruitment</h3>
+        <p>So you’re considering recruiting a new member of staff and want to understand an apprenticeship route. Here at Inspire Norfolk we can take a look at your business needs and identify where an apprentice could really help you. We’ll create a job description, find an appropriate training provider, identify funding streams, advertise your post, shortlist, facilitate interviews and then give feedback to those people who attended. We like to attend the interview with you so that we can answer any questions for the candidate and also give valuable feedback. In our opinion it’s so important that those applying for jobs, whether young or old, know where they did well and things they need to work on, so that they can go into their next interview better informed if they were unsuccessful. We take care of all of the “thanks, but no thanks calls”, and liaise with the training provider on your behalf. We can provide all the paperwork you require from offer letters to contracts of employments. If you want you only need to tell us what you want, sit back get on with doing your job and running your business and then turn up at the interview we’ll do all the hard work for you. We’re happy if you want to be more involved and we will work with you to develop each step. So if you think you could do with a little help give us a call and find out more.</p>
+    </div>
+    <div class="container flex">
+        <h3>International Work Placements</h3>
+        <p>Inspire Norfolk has been working in partnership with Norfolk County Council since 2012 to support international students coming to Norwich on work placements. Currently funded by Erasmus, this project sees students in a variety of placements with local companies across a range of sectors including retail, business, the trades and architectural services. Placements typically last between 4 and 13 weeks and the students improve their language skills, technical knowledge and self-confidence during their time with us.
+
+The friendships between individual staff and students and business to business is fascinating to watch. Each year this project grows and will in the future hopefully include our young people going out to various countries to take part in a reciprocal agreement, so watch this space.</p>
+    </div>
+    <div class="container purple-bg flex">
+        <h3>Bespoke Support</h3>
+        <p>This area can cover a wide range of work. It is really important to us here at Inspire Norfolk that a young person or new employee navigates their route into work in a really positive way. We aim to contact everyone that applies for roles and we give feedback to everyone who attends an interview in person. Without this young people may flounder on their journey. Some people however need further support and we offer a range of services. We can work with individuals to help fine tune their CV, to work on their interview skills or increase their self-confidence prior to finding a job.
+
+We also work with people who are already in the work place but may be struggling with a range of difficulties from disabilities, to chronic illness to mental health concerns. If this is you or you know someone like this or you have employees that you are concerned about please give the team a call to discuss your specific needs. We are here to help.</p>
+    </div>
+    <div class="container flex">
+        <h3>Inspire Norfolk. Leading a generation into work.</h3>
+        <p>In our opinion it’s so important that young people know where they did well and things they need to work on, so that they can go into their next interview better informed if they were unsuccessful. If you want you only need to tell us what you what, sit back relax and then turn up at the interview we’ll do all the hard work for you. We’re happy if you want to be more involved and we will work with you to develop each step.</p>
+    </div>
+     <div class="get-in-touch container">
+            <p>So if you think you could do with a little help give our experts a call today and find out more how we can find you an apprentice.</p>
+            <a class="get-in-touch-btn">Get In Touch</a>
+        </div>
 </div>
