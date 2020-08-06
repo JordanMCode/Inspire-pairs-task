ea40784aa62a37dabd51444c1cbe397430be3040
JordanMCode <66785212+JordanMCode@users.noreply.github.com>
2020-08-06 16:13:33 +0100

    Media queries
    Landing page main media queries.

--- pages/landing-page.php
+++ pages/landing-page.php
@@ -1,6 +1,7 @@
 <div class="landing-page">
     <div class="banner">
-            <div class="banner-content container">
+        <div class="banner-bg">
+            <div class="banner-content banner-container">
                 <h1>Inspire 
                     <br>
                 Norfolk</h1>
@@ -11,6 +12,7 @@
                 <a class="contact-button">Contact Us Today</a>
             </div>
         </div>
+    </div>
     <div>
         <div class=" welcome container">
             <h3>Set up in 2012 Inspire Norfolk supports businesses to recruit apprentices</h3>
@@ -18,45 +20,47 @@
             <p>We are there for a business every step of the way right up to the time they appoint a successful candidate and beyond. Our aim is that all the apprentices we place will eventually move into a full time position within the company once their apprenticeship is complete.</p>
             <p>We also work with young people themselves, mainly those who are struggling, for a variety of reasons, to find work. We will take time to get to know them and discuss their goals, likes and dislikes and create a bespoke package of support to ensure they find the right opportunity to ensure they have a positive experience and move into an appropriate apprenticeship.</p>
         </div>
-        <div class="services container">
-            <div class="service-text">
-                <h3>Our Services</h3>
-                <hr>
-                <p>At Inspire Norfolk, we have numerous different types of apprentice opportunities, meaning we can guarantee you get the apprenticeship qualifications you want in the right industry. Find out further details below.</p>
-            </div>
-            <div class="service-card">
-                <div class="service-card-content">
-                    <div class="service-card-img-1">
+        <div class="bg-container-purple">
+            <div class="services container">
+                <div class="service-text">
+                    <h3>Our Services</h3>
+                    <hr>
+                    <p>At Inspire Norfolk, we have numerous different types of apprentice opportunities, meaning we can guarantee you get the apprenticeship qualifications you want in the right industry. Find out further details below.</p>
+                </div>
+                <div class="service-card" id="service-card-1">
+                    <div class="service-card-content">
+                        <div class="service-card-img-1">
 
-                    </div>
-                    <div class="service-card-text">
-                        <span class="icon"><i class="far fa-handshake"></i></span>
-                        <strong><h4>Apprentice Recruitment</h4></strong>
-                        <p>We support businesses every step of the way when recruiting apprentices.</p>
+                        </div>
+                        <div class="service-card-text">
+                            <span class="icon"><i class="far fa-handshake"></i></span>
+                            <strong><h4>Apprentice Recruitment</h4></strong>
+                            <p>We support businesses every step of the way when recruiting apprentices.</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="service-card">
-                <div class="service-card-content">
-                    <div class="service-card-img-2">
+                <div class="service-card" id="service-card-2">
+                    <div class="service-card-content">
+                        <div class="service-card-img-2">
 
-                    </div>
-                    <div class="service-card-text">
-                        <span class="icon"><i class="fas fa-globe-americas"></i></span>
-                        <strong><h4>International Work Placements</h4></strong>
-                        <p>Students from a range of countries stay in Nofolk to complete work placements in a variety of sectors.</p>
+                        </div>
+                        <div class="service-card-text">
+                            <span class="icon"><i class="fas fa-globe-americas"></i></span>
+                            <strong><h4>International Work Placements</h4></strong>
+                            <p>Students from a range of countries stay in Nofolk to complete work placements in a variety of sectors.</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="service-card">
-                <div class="service-card-content">
-                    <div class="service-card-img-3">
+                <div class="service-card" id="service-card-3">
+                    <div class="service-card-content">
+                        <div class="service-card-img-3">
 
-                    </div>
-                    <div class="service-card-text">
-                        <span class="icon"><i class="fas fa-users"></i></span>
-                        <strong><h4>Bespoke Support</h4></strong>
-                        <p>Individually tailored support for those applying for work or who are already in work but may be struggling.</p>
+                        </div>
+                        <div class="service-card-text">
+                            <span class="icon"><i class="fas fa-users"></i></span>
+                            <strong><h4>Bespoke Support</h4></strong>
+                            <p>Individually tailored support for those applying for work or who are already in work but may be struggling.</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -65,9 +69,11 @@
             <h3>Working in partnership with Norfolk County Council since 2012</h3>
             <p>We support international students coming to Norwich on work placements. These placements are with a variety of local companies across a range of sectors including retail, business, the trades and architectural services. Each year this project grows and will in the future include our young people going out to France and Norway, so watch this space.</p>
         </div>
-        <div class="get-in-touch container">
-            <p>So if you think you could do with a little help give our experts a call today and find out more how we can find you an apprentice.</p>
-            <a class="get-in-touch-btn">Get In Touch</a>
+        <div class="bg-container-purple">
+            <div class="get-in-touch container">
+                <p>So if you think you could do with a little help give our experts a call today and find out more how we can find you an apprentice.</p>
+                <a class="get-in-touch-btn">Get In Touch</a>
+            </div>
         </div>
     </div>
 </div>
